f57fdaedf32b262b9e4267322ff9d227351df243
Andebet Tilahun <158208005+andebetpython@users.noreply.github.com>
2024-03-25 13:51:19 +0300

    replacing_string_charackter.py

--- ch.py
+++ ch.py
@@ -2,9 +2,7 @@ import random
 def modify(strings,k):
     for i in range(k):
         m=random.choice(strings)
-        #print(m)
         n=random.choice("ABCDEFGHI")
-        #print(n)
         new_strings=strings.replace(m,n)
     return new_strings
 st="andebet"
