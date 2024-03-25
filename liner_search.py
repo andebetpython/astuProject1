ada7caf109b548bb16bf88c6b82c622905722629
Andebet Tilahun <158208005+andebetpython@users.noreply.github.com>
2024-03-25 14:42:21 +0300

    linear_search.py

--- liner_search.py
+++ liner_search.py
@@ -1,14 +1,8 @@
-import  time
-start=time.time()
 def linear_search(lst,target):
     for i in range(len(lst)-1):
         if lst[i]==target:
             return i
+    return None
 
 m=[1,2,3,4,5]
-
-end=time.time()
-print(end)
-print(end-start)
-
 print(linear_search(m,4))
