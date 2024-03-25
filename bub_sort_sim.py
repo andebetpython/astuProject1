5c02b51029615e6412929c13f9dfec176bde9a33
Andebet Tilahun <158208005+andebetpython@users.noreply.github.com>
2024-03-25 13:48:06 +0300

    Update bub_sort_sim.py
    this a program for bubble sort algorithm

--- bub_sort_sim.py
+++ bub_sort_sim.py
@@ -1,6 +1,8 @@
+def bubble_sort(array):
+    for j in range(len(l)):
+        for i in range(len(l)-1):
+            if l[i]>l[i+1]:
+                l[i],l[i+1]=l[i+1],l[i]
+    return array
 l=[6,4,5,3,2,8]
-for j in range(len(l)):
-    for i in range(len(l)-1):
-        if l[i]>l[i+1]:
-            l[i],l[i+1]=l[i+1],l[i]
-print(l)
+print(bubble_sort(l))
